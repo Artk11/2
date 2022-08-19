5f1eb1a22b12820e18ab6ad12322e451c5cf45db
Artem <102547091+Artk11@users.noreply.github.com>
2022-08-19 23:53:34 +0300

    feat
    Выполнено 4.5 задания

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var cell = document.querySelector("cell");
   button.addEventListener("click", function() {
     console.log("Clicked cell: X");
-  });
+});
+function refreshPage(){
+  window.location.reload();
+}
