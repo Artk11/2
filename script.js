9df5f76fe8b2ee1937673aba2d95d55d36b30999
Artem <102547091+Artk11@users.noreply.github.com>
2022-08-23 23:58:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var cell = document.querySelector("cell");
-  button.addEventListener("click", function() {
-    console.log("Clicked cell: X");
-});
-function refreshPage(){
-  window.location.reload();
-}
