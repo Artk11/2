a608c7c8073ac7c06a9c7ac2efd6752b8ed34de8
Artem <102547091+Artk11@users.noreply.github.com>
2022-08-23 23:49:39 +0300

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,1 +1,27 @@
+let cell = document.querySelector("cell");
+  button.addEventListener("click", function() {
+    console.log("Clicked cell: X");
+});
+function refreshPage(){
+  window.location.reload();
+};
+function StartTheGame() {
+  document.getElementById('example_4').style.display='block';
+  document.getElementById('example_1').style.display='block';
+  document.getElementById('example_3').style.display='none';
+  document.getElementById('example_2').style.display='none';
+}
+function mainmenu() {
+document.getElementById('example_3').style.display='block'
+document.getElementById('example_2').style.display='block'
+document.getElementById('example_1').style.display='none'
+document.getElementById('example_4').style.display='none'
+}
 
+jQuery(document).readyfunction() 
+  let mass = new Array(); 
+  let mass_x = new Array();
+  let mass_o = new Array();
+  let mass_1_1 = x;
+  let mass_2_2 = [ [null, null, null], [null, null, null], [null, null, null] ];
+  let mass_3_3 = new Array();
