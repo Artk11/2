a5b047f049b7e0aa78b2ab2165eb5af802757ed8
Artem <102547091+Artk11@users.noreply.github.com>
2022-09-09 00:56:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,26 @@ document.getElementById('example_2').style.display='block'
 document.getElementById('example_1').style.display='none'
 document.getElementById('example_4').style.display='none'
 }
-let activeplayer = "cross";
+let activeplayer = "x"; {}
 
 
-let field = [ [null, null, null], [null, null, null], [null, null, null] ];
+let field = [ [null, null, null], [null, null, null], [null, null, null] ]; {
+  for (let i = 0; i < arr.length; i++) {
+    alert( field[i] );
+    if (i = null) {
+      document.getElementById('cross').style.display='none';
+      document.getElementById('zero').style.display='none';
+    }
+    if (i = x) {
+      document.getElementById('cross').style.display='block';
+    }
+    if (i = o) {
+      document.getElementById('zero').style.display='block';
+    }
+  }
+}
+let data-cell-row = [ [0, 0, 0], [1,1,1], [2,2,2]]; {
+  for (let i = 0; i < arr.length; i++) {
+     alert( data-cell-row[i] );
+  }
+}
